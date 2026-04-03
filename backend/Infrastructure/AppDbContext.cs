4480da227b7730f0a879b04682d4125b18f94ecb
Alvin Amaya <amayapinedaalvinasael@gmail.com>
2026-04-02 23:48:44 -0600

    Feat: Repository, DbContext

---
+++ backend/Infrastructure/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using backend.Features.Clients;
+using backend.Features.Employees;
+using backend.Features.Reservations;
+using backend.Features.Rooms;
+using backend.Features.RoomTypes;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Infrastructure
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Tables
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+    }
+}
