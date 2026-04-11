114102ab6cd5ee0fad7ff29fc6ec567b3eb25e26
LoximtoMedina <crjosuem911@gmail.com>
2026-04-11 13:21:52 -0600

    refactor: reset and re-initialize angular workspace

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { credentialsInterceptor } from './core/interceptors/credentials.interceptor';
-import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([credentialsInterceptor, errorInterceptor])),
-  ],
+    provideRouter(routes)
+  ]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,3 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'clients',
-    loadComponent: () =>
-      import('./features/clients/components/list.component').then((m) => m.ClientComponent),
-  },
-  {
-    path: '',
-    redirectTo: 'clients',
-    pathMatch: 'full',
-  },
-];
+export const routes: Routes = [];
